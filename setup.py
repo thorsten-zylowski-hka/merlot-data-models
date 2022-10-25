61b061f516580e8250390931e137726d1a0f9403
Thorsten Zylowski <zyth0001@ads.hs-karlsruhe.de>
2022-10-25 12:13:16 +0200

    auto package finding

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='merlot-data-models',
@@ -6,5 +6,5 @@ setup(
    description='Models of the Merlot project used in the APIs and applications',
    author='HKA',
    author_email='foomail@foo.example',
-   packages=['merlot-data-models']
+   packages=find_packages()
 )
