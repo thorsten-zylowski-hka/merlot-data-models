d6c89cfaa1a3a7d4b2bf4079f5242c3fca53f359
Thorsten Zylowski <zyth0001@ads.hs-karlsruhe.de>
2022-10-25 10:41:09 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='merlot-data-models',
+   version='1.0.0',
+   description='Models of the Merlot project used in the APIs and applications',
+   author='HKA',
+   author_email='foomail@foo.example',
+   packages=['merlot-data-models']
+)
