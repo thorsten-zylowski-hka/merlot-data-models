4b04fe8716824617fd1c75d9a66f3765ab09917c
Thorsten Zylowski <zyth0001@ads.hs-karlsruhe.de>
2023-03-15 16:15:01 +0100

    removed start and end point from path

--- merlot_data_models/core/educational_path.py
+++ merlot_data_models/core/educational_path.py
@@ -4,6 +4,4 @@ from typing import Optional, List
 from merlot_data_models.core.educational_step import EducationalStep
 
 class EducationalPath(BaseModel):
-    start: EducationalStep
-    end: EducationalStep
-    stations: Optional[List[EducationalStep]]
+    educationalSteps: List[EducationalStep]
