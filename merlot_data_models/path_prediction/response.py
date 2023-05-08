c015ea3202f6e47d571eaa645af188b78035ff5f
thorsten-zylowski-hka <thorsten.zylowski@h-ka.de>
2023-05-08 09:56:33 +0200

    added formal educational paths to response model
    added formal educational paths to response model

--- merlot_data_models/path_prediction/response.py
+++ merlot_data_models/path_prediction/response.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 from typing import List
 
 from merlot_data_models.core.educational_path import EducationalPath
+from merlot_data_models.core.formal_educational_step import FormalEducationalStep
 
 class PathPredictionResponse(BaseModel):
     educationals_paths: List[EducationalPath]
+    formal_educational_paths: List[List[FormalEducationalStep]]
