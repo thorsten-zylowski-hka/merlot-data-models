7f01c3867b79168cf3d80f0125fb09b64ac4245f
thorsten-zylowski-hka <thorsten.zylowski@h-ka.de>
2023-05-08 09:15:24 +0200

    moved formal educational step here
    moved formal educational step here
    added prediction configurations

---
+++ merlot_data_models/core/formal_educational_step.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class FormalEducationalStep(Enum):
+    NONE = "none"
+    GRUNDSCHULABSCHLUSS = "grundschulabschluss"
+    FACHGEBUNDENE_HOCHSCHULREIFE = "fachgebundene hochschulreife"
+    BERUFSKOLLEG = "berufskolleg"
+    BERUFSOBERSCHULE = "berufsoberschule"
+    HOCHSCHULREIFE = "hochschulreife"
+    MASTER = "master"
+    BERUFSAUFBAUSCHULE = "berufsaufbauschule"
+    BERUFSFACHSCHULE = "berufsfachschule"
+    BACHELOR = "bachelor"
+    HAUPTSCHULABSCHLUSS = "hauptschulabschluss"
+    BERUFSAUSBILDUNG = "berufsausbildung"
+    FACHHOCHSCHULREIFE = "fachhochschulreife"
+    FACHSCHULE = "Fachschule"
+    MITTLERER_BILDUNGSABSCHLUSS = "mittlerer bildungsabschluss"
+    STAATSEXAMEN = "staatsexamen"
+    JOB = "job"
+    WEITERFUEHRENDE_BERUFSQUALIFIZIERUNG = "weiterführende berufsqualifizierung"
+    PROMOTION = "promotion"
--- merlot_data_models/path_prediction/request.py
+++ merlot_data_models/path_prediction/request.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel
+from typing import List, Optional, Dict
 
 from merlot_data_models.core.cognitive_position import CognitivePosition
+from merlot_data_models.core.educational_step import EducationalStep
+from merlot_data_models.core.formal_educational_step import FormalEducationalStep
 
+
+class ExplanationConfiguration(BaseModel):
+    calculateExplanations: bool
+    calculateAlternatives: bool
+    calculateFeatureImportance: bool
+
+class PathPredictionRequestConfiguration(BaseModel):
+    explanations: Optional[ExplanationConfiguration]
+
+class PathPredictionHints(BaseModel):
+    formalEducationalPath: Optional[List[FormalEducationalStep]]
+    educationalSteps: Optional[Dict[FormalEducationalStep, EducationalStep]]
 
 class PathPredictionRequest(BaseModel):
     cognitivePosition: CognitivePosition
+    configuration: Optional[PathPredictionRequestConfiguration]
+    hints: Optional[PathPredictionHints]
