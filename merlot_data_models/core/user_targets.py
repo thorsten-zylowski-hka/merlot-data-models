973a192d0eb3e0d113424722c56873546f432298
thorsten-zylowski-hohenheim <thorsten.zylowski@uni-hohenheim.de>
2023-10-17 09:58:41 +0200

    added preferred location

--- merlot_data_models/core/user_targets.py
+++ merlot_data_models/core/user_targets.py
@@ -2,12 +2,19 @@ from typing import List, Optional
 
 from pydantic import BaseModel
 
+from merlot_data_models.core.place import Place
+
 class EducationalTarget(BaseModel):
     fieldOfSpecification: List[str]
     target: List[str]
+
+class PreferredLocation(BaseModel):
+    utterance: Optional[str]
+    location: Optional[Place]
 
 class UserTargets(BaseModel):
     educationalTarget: EducationalTarget
     careerTargets: List[str]
     positionChangeReasons: Optional[List[str]]
     personalRequirements: Optional[List[str]]
+    preferredLocations = Optional[List[PreferredLocation]]
